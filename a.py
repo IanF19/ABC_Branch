5dd277766f050f50f92bde844ef3f046450a3443
IanF19 <ianfrancoolea@gmail.com>
2025-03-06 18:08:27 -0600

    change: a.py

--- a.py
+++ a.py
@@ -12,20 +12,4 @@ def calculadora():
     print("1. Sumar")
     print("2. Restar")
 
-    operacion = input("Ingresa el número de la operación que deseas realizar (1 o 2): ")
-
-    # Solicitar números al usuario
-    num1 = float(input("Ingresa el primer número: "))
-    num2 = float(input("Ingresa el segundo número: "))
-
-    if operacion == "1":
-        resultado = suma(num1, num2)
-        print(f"El resultado de la suma es: {resultado}")
-    elif operacion == "2":
-        resultado = resta(num1, num2)
-        print(f"El resultado de la resta es: {resultado}")
-    else:
-        print("Opción no válida.")
-
-# Ejecutar la calculadora
-calculadora()
+   
